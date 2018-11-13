b1ddb37d659b837cafffd99ac67cdcecbae1c082
Andy-i3 <interwebandy@gmail.com>
2018-11-13 16:40:18 +0300

    JUNIT

---
+++ src/main/java/rushydro/ru/QdataTest.java
@@ -0,0 +1,14 @@
+package rushydro.ru;
+
+import static org.junit.Assert.*;
+
+public class QdataTest {
+
+    @org.junit.Test
+    public void main() {
+    }
+
+    @org.junit.Test
+    public void parseCurrentJson() {
+    }
+}
